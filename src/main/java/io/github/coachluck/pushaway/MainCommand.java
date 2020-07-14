8c38078421b5ef93f8e7208384fd333b3defff42
CoachLuck <beastlyaj01@gmail.com>
2020-07-14 04:31:40 -0400

    Bump to v1.0.1

--- src/main/java/io/github/coachluck/pushaway/MainCommand.java
+++ src/main/java/io/github/coachluck/pushaway/MainCommand.java
@@ -1,3 +1,23 @@
+/*
+ *     File: MainCommand.java
+ *     Last Modified: 7/14/20, 4:30 AM
+ *     Project: PushAway
+ *     Copyright (C) 2020 CoachL_ck
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.coachluck.pushaway;
 
 import org.bukkit.Bukkit;
@@ -18,7 +38,6 @@ public class MainCommand implements CommandExecutor {
         final String giveMsg = plugin.getConfig().getString("Messages.Given-Wand");
         String giveOtherMsg = plugin.getConfig().getString("Messages.Give-Other-Wand");
         if(args.length == 0) {
-            // TODO Send help
             if(sender.hasPermission("pushaway.use")) {
 
                 sender.sendMessage(ItemUtil.format("&8&m         [ &ePushAway &8]&m         "));
@@ -63,7 +82,7 @@ public class MainCommand implements CommandExecutor {
         }
         if(args.length == 2) {
             if(!args[0].toLowerCase().startsWith("g")) {
-                // TODO : Unkown Command
+                sender.sendMessage("&cUnkown command! &eTry /pushaway for help.");
                 return true;
             }
             if(!sender.hasPermission("pushaway.givewand.others")) {
@@ -71,6 +90,10 @@ public class MainCommand implements CommandExecutor {
                 return true;
             }
             Player target = Bukkit.getPlayerExact(args[1]);
+            if(target == null) {
+                sender.sendMessage("&c" + args[1] + " &7is currently offline.");
+                return true;
+            }
             target.getInventory().addItem(plugin.wand);
             target.sendMessage(ItemUtil.format(giveMsg));
             sender.sendMessage(ItemUtil.format(giveOtherMsg.replaceAll("%player%", target.getName())));
