978320082f020591b5ef8350cedd49a96ea741e8
CoachLuck <beastlyaj01@gmail.com>
2020-07-19 18:18:10 -0400

    Refractor and Code Style improvement

--- src/main/java/io/github/coachluck/pushaway/RightClickListener.java
+++ src/main/java/io/github/coachluck/pushaway/RightClickListener.java
@@ -1,6 +1,6 @@
 /*
  *     File: RightClickListener.java
- *     Last Modified: 7/19/20, 5:53 PM
+ *     Last Modified: 7/19/20, 6:17 PM
  *     Project: PushAway
  *     Copyright (C) 2020 CoachL_ck
  *
@@ -34,6 +34,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.Vector;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 public class RightClickListener implements Listener {
@@ -46,14 +48,11 @@ public class RightClickListener implements Listener {
     @EventHandler
     public void onRightClick(PlayerInteractEvent e) {
         if((e.getAction() != Action.RIGHT_CLICK_AIR) && (e.getAction() != Action.RIGHT_CLICK_BLOCK)) return;
-        int slotId = e.getPlayer().getInventory().getHeldItemSlot();
+        final int slotId = e.getPlayer().getInventory().getHeldItemSlot();
 
-        ItemStack heldItem = e.getPlayer().getInventory().getItem(slotId);
-        if(heldItem.getType() != plugin.wand.getType()) return;
-
-        ItemMeta heldItemMeta = heldItem.getItemMeta();
-        if(heldItemMeta.getDisplayName() != plugin.wand.getItemMeta().getDisplayName()) return;
-        if(heldItemMeta.getLore() != plugin.wand.getItemMeta().getLore()) return;
+        final ItemMeta heldItemMeta = e.getPlayer().getInventory().getItem(slotId).getItemMeta();
+        if(!heldItemMeta.getDisplayName().equals(plugin.wand.getItemMeta().getDisplayName())) return;
+        if(!heldItemMeta.getLore().equals(plugin.wand.getItemMeta().getLore())) return;
 
         e.setCancelled(true);
         final Player player = e.getPlayer();
@@ -68,63 +67,77 @@ public class RightClickListener implements Listener {
             return;
         }
 
+        doLaunch(player);
+        return;
+    }
+
+    private void doLaunch(Player player) {
+        final UUID pUUID = player.getUniqueId();
         final int range = plugin.getConfig().getInt("Push-Away-Range");
-        final double launchX = plugin.getConfig().getDouble("Push-Away-Launch-X");
-        final double launchY = plugin.getConfig().getDouble("Push-Away-Launch-Y");
-        int playerCount = 0;
+        int playerCount = launchSurroundingPlayers(player, player.getNearbyEntities(range, range, range));
 
-        for (Entity entity : player.getNearbyEntities(range, range, range)) {
-            if (entity instanceof Player) {
-                playerCount++;
-                Player p = (Player) entity;
-
-                final Location playerCenterLocation = player.getEyeLocation();
-                final Location playerToThrowLocation = p.getEyeLocation();
-
-                final double x = playerToThrowLocation.getX() - playerCenterLocation.getX();
-                final double y = playerToThrowLocation.getY() - playerCenterLocation.getY();
-                final double z = playerToThrowLocation.getZ() - playerCenterLocation.getZ();
-
-                final Vector throwVector = new Vector(x, y, z);
-
-                throwVector.normalize();
-                throwVector.multiply(launchX);
-                throwVector.setY(launchY);
-
-                p.setVelocity(throwVector);
-                p.sendMessage(ItemUtil.format(
-                        plugin.getConfig().getString("Messages.Pushed-By-Player")
-                            .replaceAll("%player%", player.getName())
-                ));
-
-                if(plugin.getConfig().getBoolean("Sound.When-Pushed.Enabled")) {
-                    p.playSound(p.getLocation(),
-                            Sound.valueOf(plugin.getConfig().getString("Sound.When-Pushed.Sound")),
-                            (float) plugin.getConfig().getDouble("Sound.When-Pushed.Volume"),
-                            (float) plugin.getConfig().getDouble("Sound.When-Pushed.Pitch"));
-                }
-                // TODO : Add particle effects
-            }
-        }
-
-        if(playerCount > 0) {
-            if (plugin.getConfig().getBoolean("Sound.When-Pushed.Enabled")) {
-                player.playSound(player.getLocation(),
-                        Sound.valueOf(plugin.getConfig().getString("Sound.On-Push.Sound")),
-                        (float) plugin.getConfig().getDouble("Sound.On-Push.Volume"),
-                        (float) plugin.getConfig().getDouble("Sound.On-Push.Pitch"));
-            }
-
-            plugin.cooldowns.put(pUUID, new Cooldown(pUUID, plugin.getConfig().getInt("Usage-Cooldown")));
+        if(playerCount == 0) {
             player.sendMessage(ItemUtil.format(
-                    plugin.getConfig().getString("Messages.Pushed-Players")
-                        .replaceAll("%num%", Integer.toString(playerCount))
+                    plugin.getConfig().getString("Messages.No-Players-Nearby")
             ));
             return;
         }
+        if (plugin.getConfig().getBoolean("Sound.When-Pushed.Enabled")) {
+            player.playSound(player.getLocation(),
+                    Sound.valueOf(plugin.getConfig().getString("Sound.On-Push.Sound")),
+                    (float) plugin.getConfig().getDouble("Sound.On-Push.Volume"),
+                    (float) plugin.getConfig().getDouble("Sound.On-Push.Pitch"));
+        }
 
+        plugin.cooldowns.put(pUUID, new Cooldown(pUUID, plugin.getConfig().getInt("Usage-Cooldown")));
         player.sendMessage(ItemUtil.format(
-                plugin.getConfig().getString("Messages.No-Players-Nearby")
+                plugin.getConfig().getString("Messages.Pushed-Players")
+                        .replaceAll("%num%", Integer.toString(playerCount))
         ));
+    }
+
+    private int launchSurroundingPlayers(Player player, List<Entity> nearbyPlayers) {
+        int playerCount = 0;
+        Iterator<Entity> it = nearbyPlayers.iterator();
+        for (Entity entity : nearbyPlayers) {
+            if (entity instanceof Player) {
+                playerCount++;
+                Player target = (Player) entity;
+                launchPlayer(player, target);
+                // TODO : Add particle effects
+            }
+        }
+        return playerCount;
+    }
+
+    private void launchPlayer(Player player, Player target) {
+        final double launchX = plugin.getConfig().getDouble("Push-Away-Launch-X");
+        final double launchY = plugin.getConfig().getDouble("Push-Away-Launch-Y");
+        final Location playerCenterLocation = player.getEyeLocation();
+        final Location playerToThrowLocation = target.getEyeLocation();
+
+        final double x = playerToThrowLocation.getX() - playerCenterLocation.getX();
+        final double y = playerToThrowLocation.getY() - playerCenterLocation.getY();
+        final double z = playerToThrowLocation.getZ() - playerCenterLocation.getZ();
+
+        final Vector throwVector = new Vector(x, y, z);
+
+        throwVector.normalize();
+        throwVector.multiply(launchX);
+        throwVector.setY(launchY);
+
+        target.setVelocity(throwVector);
+        target.sendMessage(ItemUtil.format(
+                plugin.getConfig().getString("Messages.Pushed-By-Player")
+                        .replaceAll("%player%", player.getName())
+        ));
+
+        if(plugin.getConfig().getBoolean("Sound.When-Pushed.Enabled")) {
+            target.playSound(target.getLocation(),
+                    Sound.valueOf(plugin.getConfig().getString("Sound.When-Pushed.Sound")),
+                    (float) plugin.getConfig().getDouble("Sound.When-Pushed.Volume"),
+                    (float) plugin.getConfig().getDouble("Sound.When-Pushed.Pitch"));
+        }
+        // TODO : Add particle effects
     }
 }
